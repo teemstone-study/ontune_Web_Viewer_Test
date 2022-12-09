599bb6bbf82e0da4e509e160fe7a55889360201c
gjrlwjs <shinsh@ontune.co.kr>
2022-12-09 10:35:51 +0900

    Init

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import App from './App.svelte';
+
+// const app = new App({
+// 	target: document.body,
+// 	props: {
+// 		name: 'world'
+// 	}
+// });
+
+
+
+// Tree 영역
+const app = new App({
+	target: document.getElementById("div_tree")
+});
+
+
+// 제니퍼 영역
+
+
+// 모자이크 영역
+
+
+export default app;

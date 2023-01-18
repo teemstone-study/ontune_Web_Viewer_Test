e56296f516fdd52786d899e50157b9a1da542080
gyupilsim <garleyqp@gmail.com>
2023-01-18 17:53:54 +0900

    로그 제거, pie는 32 -> 64로 변경

--- src/websocketWorker.svelte
+++ src/websocketWorker.svelte
@@ -8,7 +8,7 @@
   let workEvent;
     onMount(() => {
         console.log('workerInit');
-        let workEvent = new Worker('dataRecever.js');
+        workEvent = new Worker('dataRecever.js');
         workEvent.onmessage = updateItemList;
         WebSendData.subscribe((value) => {
             if (Number.isInteger(value))
@@ -18,17 +18,10 @@
         });
 
         Data_Infos.subscribe((Item) => {
-        console.log("items ~~~~~~~~~~~");
-        console.log(Item);
         // console.log(get(Data_Infos));
 
         if (Item.BASIC_CODE != null) {
-            console.log("=============load Key Flag S ============");
-
             let tmp_Flag = 2;
-
-            console.log(Item.LASTPERF_CODE.Flag_YN);
-            console.log(Item.LASTPERF_CODE.Code);
 
             tmp_Flag += Item.LASTPERF_CODE.Flag_YN === true ? Item.LASTPERF_CODE.Code:0;
             tmp_Flag += Item.BASIC_CODE.Flag_YN    === true ? Item.BASIC_CODE.Code:0;
@@ -37,7 +30,7 @@
             tmp_Flag += Item.NET_CODE.Flag_YN      === true ? Item.NET_CODE.Code:0;
             tmp_Flag += Item.DISK_CODE.Flag_YN     === true ? Item.DISK_CODE.Code:0;
 
-            console.log(tmp_Flag);
+            //console.log(tmp_Flag);
             console.log("=============load Key Flag E ============");
 
             // console.log("==============Flag Change and Send Data===========");
@@ -57,35 +50,37 @@
                     WebReceiveData.set({code : Parsedata.code, data : Parsedata.data});
                     break;
                 case 4:
-                    console.log("Code 4 Data Recv ====== Lastperf ====== Grid");
+                    //console.log("Code 4 Data Recv ====== Lastperf ====== Grid");
                     // console.log(Parsedata.data);
                     WebReceiveGridData.set(Parsedata.data);
                     break;                
                 case 8:
-                    console.log("Code 8 Data Recv ====== Basic ====== Bar");
+                    //console.log("Code 8 Data Recv ====== Basic ====== Bar");
                     // console.log(Parsedata.data);
                     WebReceiveBarData.set(Parsedata.data);
                     break;
                 case 16:
-                    console.log("Code 16 Data Recv ====== CPU ====== Line");
+                    //console.log("Code 16 Data Recv ====== CPU ====== Line");
                     // console.log(Parsedata.data);
                     WebReceiveLineData.set(Parsedata.data);
                     break;                
                 case 32:
-                    console.log("Code 32 Data Recv ====== Mem ====== Pie");
+                    console.log("Code 34 Data Recv");
+                    break;                
+                case 64:
+                    //console.log("Code 64 Data Recv ====== Mem ====== Pie");
 
                     let arrayData = String(Parsedata.data).split(",");
-                    console.log(arrayData[3]);
-                    console.log(arrayData[4]);
+                    let num1 = 0;
+                    let num2 = 0;
+                    if (Number.isNaN(arrayData[3]) === false)
+                        num1 = Number(arrayData[3]);
 
-                    let num1 = Number(arrayData[3]);
-                    let num2 = Number(arrayData[4]);
+                    if (Number.isNaN(arrayData[4]) === false)
+                        num2 = Number(arrayData[4]);
 
                     WebReceivePieData.set({value1: num1,value2: num2});
                     break;
-                case 64:
-                    console.log("Code 64 Data Recv");
-                    break;                
                 case 128:
                     console.log("Code 128 Data Recv");
                     break;                                
